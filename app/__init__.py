3df33b46c9bb81c90c82a95577104cb068cae010
Nathan Brown <spades@live.no>
2019-04-09 04:02:08 -0400

    Added webpack and started using react

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,17 @@ from flask_pymongo import PyMongo
 import os
 from dotenv import load_dotenv
 
-load_dotenv('.env')
+load_dotenv(".env")
 mongoUri = os.getenv("MONGO_DB_URI")
 
-#from . import config_default
-#config = config_default.DevConfig
-app = Flask(__name__)
+# from . import config_default
+# config = config_default.DevConfig
+app = Flask(__name__, static_folder="./public", template_folder="./static")
 app.config["MONGO_URI"] = mongoUri
-app.config.from_object(os.environ.get('FLASK_ENV') or 'config_default.DevConfig')
+app.config.from_object(os.environ.get("FLASK_ENV") or "config_default.DevConfig")
 mongo = PyMongo(app)
-#app.config["DB"] = mongo
+# app.config["DB"] = mongo
 from app.routes import page_routes
 from app.routes.api.api import api_blueprint
-app.register_blueprint(api_blueprint, url_prefix='/api')
+
+app.register_blueprint(api_blueprint, url_prefix="/api")
