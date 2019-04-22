f63980d26a1b947380ba01d1ba55b8a32f4e7ca7
Nathan Brown <spades@live.no>
2019-04-21 23:39:39 -0400

    fixed uploading in data tool

--- data_tool.py
+++ data_tool.py
@@ -4,7 +4,9 @@ import os
 import json
 import time
 from dotenv import load_dotenv
-from pymongo import MongoClient
+from pymongo import MongoClient, TEXT
+
+mongoUri = os.getenv("DATA_TOOL_URI")
 def progress(count, total, status=''):
     bar_len = 50
     filled_len = int(round(bar_len * count / float(total)))
@@ -35,20 +37,25 @@ def readSegment(remaining, stream):
     data.append(b'\n')
     return b"".join(data), False, chunk[i+1:]
 
-def db_mod():
+def db_mod(data_type="EDITION"):
     load_dotenv(".env")
-    mongoUri = os.getenv("MONGO_DB_URI")
     client = MongoClient(mongoUri)
-    db = client.get_default_database()
-    db.authors.create_index("name")
-    db.authors.create_index("key")
-def upload(input):
+    db = client.book_db
+    if data_type == "AUTHOR":
+        db.authors.create_index([("name",TEXT)])
+        db.authors.create_index("key")
+    if data_type == "WORK" or data_type == "EDITION":
+        db.works.create_index([("title_cleaned",TEXT)])
+        db.works.create_index("key")
+def upload(input, data_type="EDITION"):
     load_dotenv(".env")
-    mongoUri = os.getenv("MONGO_DB_URI")
     client = MongoClient(mongoUri)
-    db = client.get_default_database()
+    db = client.book_db;
     total_size = os.path.getsize(input)
-    max_upload_size = 14000000*5
+    #max_upload_size = 1400
+    #bulk_max_size = 1400;
+    MAX_UPLOAD = False
+    max_upload_size = 14000000
     bulk_max_size = 14000000;
     size_read = 0
     lastPrinted = 0
@@ -66,28 +73,60 @@ def upload(input):
                 seg_json = json.loads(seg.decode("utf-8"))
             except json.decoder.JSONDecodeError:
                 if EOF:
-                    db.authors.insert_many(bulk)
+                    if data_type == "AUTHOR":
+                        db.authors.insert_many(bulk)
+                    if data_type == "WORK":
+                        db.works.insert_many(bulk)
+                    if data_type == "EDITION":
+                        db.editions.insert_many(bulk)
                     break
                 continue
+            if data_type == "WORK" or data_type == "EDITION":
+                if 'title' in seg_json:
+                    cleaned_title = seg_json["title"]
+                    cleaned_title = cleaned_title.replace(".","")
+                    cleaned_title = cleaned_title.replace(","," ")
+                    cleaned_title = cleaned_title.replace("'","")
+                    cleaned_title = cleaned_title.replace("?","")
+                    cleaned_title = cleaned_title.replace("\"","")
+                    cleaned_title = cleaned_title.replace("(","")
+                    cleaned_title = cleaned_title.replace(")","")
+                    seg_json["title_cleaned"] = cleaned_title
             bulk.append(seg_json)
             bulk_size += seg_size
             bulk_count += 1
-            if size_read > max_upload_size:
-                db.authors.insert_many(bulk)
-                break
+            if MAX_UPLOAD:
+                if size_read > max_upload_size:
+                    if data_type == "AUTHOR":
+                        db.authors.insert_many(bulk)
+                    if data_type == "WORK":
+                        db.works.insert_many(bulk)
+                    if data_type == "EDITION":
+                        db.editions.insert_many(bulk)
+                    break
 
             if bulk_size > bulk_max_size:
                 bulk_count = 0
-                db.authors.insert_many(bulk)
+                if data_type == "AUTHOR":
+                    db.authors.insert_many(bulk)
+                if data_type == "WORK":
+                    db.works.insert_many(bulk)
+                if data_type == "EDITION":
+                    db.editions.insert_many(bulk)
                 bulk = []
                 bulk_size = 0
 
             currentPercent = size_read/total_size
             if currentPercent-lastPrinted > 0.001:
-                progress(size_read,total_size, "CLEANED")
+                progress(size_read,total_size, "UPLOADED")
                 lastPrinted = currentPercent
             if EOF:
-                db.authors.insert_many(bulk)
+                if data_type == "AUTHOR":
+                    db.authors.insert_many(bulk)
+                if data_type == "WORK":
+                    db.works.insert_many(bulk)
+                if data_type == "EDITION":
+                    db.editions.insert_many(bulk)
                 break
 
     endTime = time.time()
@@ -95,7 +134,7 @@ def upload(input):
     print()
     print("Elapsed Seconds:")
     print(elapsed)
-def clean(input, output, data_type="AUTHOR"):
+def clean(input, output, data_type="EDITION"):
     author_elements = ["name", "personal_name", "birth_date","death_date", "key"]
     edition_elements = ["title","subtitle","key","isbn_10","isbn_13","number_of_pages","publish_date","authors","subjects"]
     work_elements = ["title","key","author","authors"]
@@ -118,7 +157,7 @@ def clean(input, output, data_type="AUTHOR"):
                 continue
 
             for element in list(seg_json.keys()):
-                if element not in author_elements:
+                if element not in work_elements:
                     seg_json.pop(element, None)
                 if data_type == "AUTHOR":
                     if element == "name":
@@ -146,8 +185,8 @@ if __name__ == "__main__":
         exit()
     run_mode = sys.argv[1]
 
-    inputFile = "A:/authors_cleaned.txt"
-    outputFile = "A:/authors_cleaned.txt"
+    inputFile = "A:/editions_cleaned.txt"
+    outputFile = "A:/editions_cleaned.txt"
     if run_mode == "CLEAN":
         clean(inputFile, outputFile)
     if run_mode == "UPLOAD":
